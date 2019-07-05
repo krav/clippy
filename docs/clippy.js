d3b8f92cf2a11d547c2b72d623e157251833fffc
Kristoffer Ravneberg <kristoffer@microdisko.no>
2019-07-05 15:50:25 +0200

    add guide

--- docs/clippy.js
+++ docs/clippy.js
@@ -81,7 +81,7 @@ const Clippy = obj => {
 
     <div class="blc_row">
         <div class="blc_btn">
-            <a target="_blank" href="https://talk.theborderland.se">
+            <a target="_blank" href="https://guide.theborderland.se">
                 <img src="https://menu.theborderland.se/guide.gif" alt="What Where When Guide" />
             </a>
         </div>
